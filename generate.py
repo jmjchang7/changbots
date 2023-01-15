4bd10ebdbd351c96d6a1fb6191878c5c836aaea2
jmjchang7 <120343561+jmjchang7@users.noreply.github.com>
2023-01-14 22:48:11 -0600

    fixed position of links/joints

--- generate.py
+++ generate.py
@@ -1,21 +1,20 @@
 import pyrosim.pyrosim as pyrosim
 
-length,width,height = 1,1,1
-x,y,z=0,0,0.5
+
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[x-2,y-2,z], size=[length,width,height])
+    pyrosim.Send_Cube(name="Box", pos=[-2,-2,0.5], size=[1,1,1])
     pyrosim.End()
 
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5], size=[length,width,height])
-    pyrosim.Send_Joint(name = "Torso_Body" , parent= "Torso" , child = "Body" , type = "revolute", position = [0.5,0,1])
-    pyrosim.Send_Cube(name="Body", pos=[0.5,0,0.5], size=[length,width,height])
-    pyrosim.Send_Joint(name = "Body_Leg" , parent= "Body" , child = "Leg" , type = "revolute", position = [1,0,0])
-    pyrosim.Send_Cube(name="Leg", pos=[0.5,0,-0.5], size=[length,width,height])
+    pyrosim.Send_Cube(name = "Torso", pos=[1,0,1.5], size=[1,1,1])
+    pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0.5,0,1])
+    pyrosim.Send_Cube(name = "BackLeg", pos=[-0.5,0,-0.5], size=[1,1,1])
+    pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [1.5,0,1])
+    pyrosim.Send_Cube(name = "FrontLeg", pos=[0.5,0,-0.5], size=[1,1,1])
     pyrosim.End()
 
 Create_World()
