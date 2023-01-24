64f760754fe7f1be5fcfb85094576a120cccbc99
jmjchang7 <120343561+jmjchang7@users.noreply.github.com>
2023-01-23 18:38:27 -0600

    updated simulation

--- simulation.py
+++ simulation.py
@@ -24,6 +24,7 @@ class SIMULATION:
             time.sleep(1/60)
             p.stepSimulation()
             self.robot.Sense(t)
+            self.robot.Think()
             self.robot.Act(t)
 
     def __del__(self):
