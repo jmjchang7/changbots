6a76e7232ab2de115b1a8240b978235a741a9719
jmjchang7 <120343561+jmjchang7@users.noreply.github.com>
2023-01-23 23:26:36 -0600

    updated neuron

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -52,9 +52,18 @@ class NEURON:
 
         return self.type == c.HIDDEN_NEURON
 
-    def Update_Hidden_Or_Motor_Neuron(self):
+    def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
         
-        self.Set_Value(math.pi/4.0)
+        self.Set_Value(0.0)
+
+        for key in synapses:
+            if key[1] == self.Get_Name():
+                self.Allow_Presynaptic_Neuron_To_Influence_Me(synapses[key].Get_Weight(), neurons[key[0]].Get_Value())
+
+        self.Threshold()
+
+    def Allow_Presynaptic_Neuron_To_Influence_Me(self, weight, value):
+        self.Add_To_Value((value * weight))
 
     def Is_Motor_Neuron(self):
 
