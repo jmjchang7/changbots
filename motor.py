f6e248e718a82a5496c7dd0081ef4598d5ba353e
jmjchang7 <120343561+jmjchang7@users.noreply.github.com>
2023-01-23 18:39:50 -0600

    updated motor

--- motor.py
+++ motor.py
@@ -8,20 +8,6 @@ class MOTOR:
         self.jointName = jointName
         self.Prepare_To_Act()
 
-    def Prepare_To_Act(self):
-        self.amplitude = c.amplitude_BL
-
-        if self.jointName == b'Torso_BackLeg':
-            self.frequency = c.frequency_BL
-        else:
-            self.frequency = c.frequency_BL/2
-
-        self.offset = c.phaseOffset_BL
-        self.motorValues = self.amplitude * numpy.sin(self.frequency * numpy.linspace(0, numpy.pi*2, 1000) + self.offset)  
-
-    def Set_Value(self, robotId, t):
+    def Set_Value(self, robotId, desiredAngle):
         pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = self.jointName, controlMode = p.POSITION_CONTROL, 
-                        targetPosition = self.motorValues[t], maxForce = 500)
-
-    def Save_Values(self):
-        numpy.save('data/motorcommands.npy', self.motorValues)
+                        targetPosition = desiredAngle, maxForce = 500)
