6c95b67bba3664d2d47efd2efb4b2f690948910b
jmjchang7 <120343561+jmjchang7@users.noreply.github.com>
2023-03-05 22:05:07 -0600

    updated sensor

--- sensor.py
+++ sensor.py
@@ -1,10 +1,11 @@
 import pyrosim.pyrosim as pyrosim
 import numpy
+import constants as c
 
 class SENSOR:
     def __init__(self, linkName):
         self.linkName = linkName
-        self.values = numpy.zeros(1000)
+        self.values = numpy.zeros(c.runTime)
 
     def Get_Value(self, t):
         self.values[t] = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
