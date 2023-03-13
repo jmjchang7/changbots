81e6408b8953d5878c8533f2203d8d4ebac87f55
jmjchang7 <120343561+jmjchang7@users.noreply.github.com>
2023-03-12 19:18:28 -0500

    updated search

--- search.py
+++ search.py
@@ -7,10 +7,14 @@ import matplotlib.pyplot as plt
 fitnessCurves = [[] for _ in range(c.numSeeds)]
 
 for i in range(1, c.numSeeds + 1):
+    random.seed(i)
     phc = PARALLEL_HILL_CLIMBER()
     phc.Evolve()
+    input("Press enter to continue...")
     phc.Show_Best()
     fitnessCurves[i-1] = phc.Get_Fitness_Curve()
+    plt.plot(fitnessCurves[i-1])
+    plt.show()
 
 # plot all curves
 for i in range(c.numSeeds):
